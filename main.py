cfa3ebe354e60c4cff7197804acb9d5b2ebb974a
alessTiboca <75087809+alessTiboca@users.noreply.github.com>
2021-10-19 09:05:32 +0300

    Probleme 1-5

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
+
+def caracterulDinLista(list):
+    caracter = ""
+    maxApariti = 0
+    caracterApariti = 0
+    apariti = 0
+    for x in list:
+        for i in range(len(x)):
+            caracter = x[i]
+            for y in list:
+                for j in range(i,len(x)):
+                    if x[j] == caracter:
+                        apariti += 1        
+
+
+def palindrom(element):
+
+    n = len(element)
+    for i in range(0, n//2):
+        if element[i] != element[n-i-1]:
+            return False
+    return True
+
+
+def elementePalindromDinLista(list):
+    """Functia afiseaza toate elementele palindroame din lista
+
+    Args:
+        list ([string]): [lista de șiruri de caractere]
+    """
+
+    listaPalindrom = []
+    for x in list:
+        if palindrom(x):
+            listaPalindrom.append(x)
+    print(listaPalindrom)
+
+
+
 def verificareListaUnica(list):
     """Functia verifica daca elementele din lista se repeta si afiseaza elementele care se repeta, in caz contrar afiseaza UNIC
 
@@ -6,9 +45,9 @@ def verificareListaUnica(list):
     """
 
     listaVerificata = []
-    for i in range(len(list)+1):
+    for i in range(len(list)):
         ok = True
-        for j in range(i, len(list)+1):
+        for j in range(i, len(list)):
             if list[i] == list[j]:
                 ok = False
         if ok:
@@ -50,14 +89,14 @@ def citireLista(list):
     return list
 
 def main():
-
-    testSirCautatInLista()
-
+    
     list = []
     while True:
         print("1. Citire lista")
         print("2. Verifica dacă un șir de caractere se găsește în listă ")
         print("3. Afiseaza elementele sirului care se repeta")
+        print("4. Afiseaza elementele sirului care sunt palindroame ")
+        print("5. Afiseaza lista in care elementele care contin caracterul cu cele mai multe apariti sunt inlocuite cu numarul respectiv ")
         print("x. Iesire\n")
         optiuneMeniu = input("Alege o optiune: ")
         if optiuneMeniu == "1":
@@ -66,7 +105,11 @@ def main():
             sirDeCautat = input("Dati sirul de caractere care trebuie cautat: \n")
             sirCautatInLista(sirDeCautat, list)  
         elif optiuneMeniu == "3":
-            verificareListaUnica(list)  
+            verificareListaUnica(list) 
+        elif optiuneMeniu == "4":
+            elementePalindromDinLista(list)  
+        elif optiuneMeniu == "5":
+            caracterulDinLista(list)     
         elif optiuneMeniu == "x":
             break
         else:
